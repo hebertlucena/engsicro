8916839a117de51089810ca89a32a06ae911b29b
hebertlucena <67700370+hebertlucena@users.noreply.github.com>
2023-06-02 16:11:17 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from datetime import datetime, timedelta
 
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
-from_time = (datetime.now() + timedelta(-1000)).strftime(TIMESTAMP_FORMAT)
+from_time = (datetime.now() + timedelta(-1)).strftime(TIMESTAMP_FORMAT)
 
 args = {
     'section' : 'education',
@@ -35,4 +35,15 @@ sections = json.loads(src)['response']
 
 df = pd.DataFrame.from_dict(response['results'])
 
-df.to_csv("education.csv")
+
+path = Path("temp/education.csv")
+
+if path.is_file() == False:
+    df.to_csv("temp/education.csv")
+else:
+    df_old = pd.read_csv("temp/education.csv")
+    df_new = pd.concat([df_old, df])
+        
+    df_main_drop_duplicadas = df_new.drop_duplicates(subset = 'id', keep = 'first')
+        
+    df_main_drop_duplicadas.to_csv("temp/education.csv")
